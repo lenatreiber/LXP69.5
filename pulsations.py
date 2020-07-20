539a828563906e9ae32686f4f49c3492994917c2
lenatreiber <50960144+lenatreiber@users.noreply.github.com>
2020-07-20 17:38:20 -0400

    Add files via upload

--- pulsations.py
+++ pulsations.py
@@ -12,6 +12,7 @@ from matplotlib.gridspec import GridSpec
 from stingray.pulse.search import phaseogram, plot_phaseogram, plot_profile
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
+from scipy.ndimage.filters import gaussian_filter
 
 def readevt(file,ind=1):
     '''
@@ -62,7 +63,7 @@ def phasehist(evt_data,pd,figsize=(8,6),color='slateblue',bins=32):
     #returns histogram (counts and bin boundary arrays), and array of middles of phase bins
     return a,mids
 
-def phasehist_sh(ed,pd,ens=['0.3-10 keV','0.3-1.5 keV','1.5-10 keV'],colors=['slateblue','palevioletred','navy','cornflowerblue'],figsize=(8,6),bins=32):
+def phasehist_sh(ed,pd,ens=['0.3-10 keV','0.3-1.5 keV','1.5-10 keV'],colors=['slateblue','palevioletred','navy','cornflowerblue'],figsize=(8,6),bins=32,epoch=0):
     '''
     Same as phasehist, but for a list of astropy tables
     
@@ -89,7 +90,7 @@ def phasehist_sh(ed,pd,ens=['0.3-10 keV','0.3-1.5 keV','1.5-10 keV'],colors=['sl
     plt.figure(figsize=figsize)
     hists = []
     for e in range(len(ed)):
-        ph = ed[e]['TIME']%pd
+        ph = (ed[e]['TIME']+ epoch)%pd
         ph_1 = ph/pd
         ph_2 = ph_1+1
         ph_3 = list(ph_1)+list(ph_2)
@@ -110,7 +111,8 @@ def phasehist_sh(ed,pd,ens=['0.3-10 keV','0.3-1.5 keV','1.5-10 keV'],colors=['sl
             
 def phaserate(hists,mids,exptime,bgb=False,bg=[],ens=['0.3-10 keV','S: 0.3-1.5 keV','H: 1.5-10 keV'],
               colors=['slateblue','maroon','navy','cornflowerblue'],figsize=(8,6),rate=False,
-              ls=['solid','solid','solid','solid','solid','solid','solid'],enhist=False,ed=[],pd=pd,enbg=[],norm_bg=[]):
+              ls=['solid','solid','solid','solid','solid','solid','solid'],enhist=False,ed=[],pd=pd,enbg=[],norm_bg=[],
+              yrange1=[0,14],yrange2=[-1,1]):
     '''
     Plots step histogram of phase, with count rate on y-axis
     
@@ -143,8 +145,8 @@ def phaserate(hists,mids,exptime,bgb=False,bg=[],ens=['0.3-10 keV','S: 0.3-1.5 k
     '''
     #calculate count rate
     if enhist:
-        fig,ax = plt.subplots(3,1,figsize=figsize,gridspec_kw = {'height_ratios':[3,4,3]})
-        plt.subplots_adjust(wspace=0, hspace=0.1)
+        fig,ax = plt.subplots(3,1,figsize=figsize,gridspec_kw = {'height_ratios':[4,3,3]})
+        plt.subplots_adjust(wspace=0, hspace=0.05)
     elif rate:
         fig,ax = plt.subplots(2,1,figsize=figsize,sharex=True)
         plt.subplots_adjust(wspace=0, hspace=0.05)
@@ -160,28 +162,34 @@ def phaserate(hists,mids,exptime,bgb=False,bg=[],ens=['0.3-10 keV','S: 0.3-1.5 k
         binb = hists[h][1] #array of bin boundaries
         if bgb: cr = (counts*bins/(2*exptime))-bg[h]
         else: cr = (counts*bins/(2*exptime))
-        if rate: hist = ax[0].hist(binb[:-1],binb,weights=cr,color=colors[h],histtype='step',linestyle=ls[h],label=ens[h])
+        if enhist: ploc = 1
+        else: ploc = 0
+        if rate: hist = ax[ploc].hist(binb[:-1],binb,weights=cr,color=colors[h],histtype='step',linestyle=ls[h],label=ens[h])
         else: hist = ax.hist(binb[:-1],binb,weights=cr,color=colors[h],histtype='step',linestyle=ls[h],label=ens[h])
         #error is square root of counts divided by time in bin
         err = np.sqrt(counts)*bins/(2*exptime) #factor of 2 since really only 1/2 the data bins; just repeated
         if rate: 
-            ax[0].errorbar(mids,hist[0],yerr=err,linestyle='none',color=colors[h])
-            ax[0].set_ylabel('Counts/s',fontsize=18)
-            ax[0].set_xlim(0,2)
-            #specific to this dataset:
-            if enhist:
-                ax[0].set_ylim(0,14)
-                ax[0].set_yticks(np.arange(0,16,2))
-                ax[0].xaxis.set_major_locator(MultipleLocator(0.5))
-                ax[0].xaxis.set_minor_locator(MultipleLocator(0.1))
+            ax[ploc].errorbar(mids,hist[0],yerr=err,linestyle='none',color=colors[h])
+            ax[ploc].set_ylabel('Rate (c/s)',fontsize=18)
+            ax[ploc].set_xlim(0,2)
+            ax[ploc].set_xticks(np.arange(0,2,.5))
+            ax[ploc].set_xticklabels(['','','','',''])
+            ax[ploc].set_ylim(yrange1)
+            ax[ploc].set_yticks(np.arange(yrange1[0],yrange1[1]+1.5,2))
+            ax[ploc].minorticks_on()
+#             if enhist:
+#                 ax[0].set_ylim(0,14)
+#                 ax[0].set_yticks(np.arange(0,16,2))
+#                 ax[0].xaxis.set_major_locator(MultipleLocator(0.5))
+#                 ax[0].xaxis.set_minor_locator(MultipleLocator(0.1))
         else: 
             ax.errorbar(mids,hist[0],yerr=err,linestyle='none',color=colors[h])
-            ax.set_ylabel('Counts/s',fontsize=14)
+            ax.set_ylabel('Rate (c/s)',fontsize=14)
         ratehists.append(hist)
         errs.append(err)
     plt.xlim(0,2)
     plt.xlabel('Phase',fontsize=18)
-    if rate: ax[0].legend(loc='lower right') #fixing for now
+    if rate: ax[ploc].legend(loc='upper right') #fixing for now
     else: ax.legend()
     if enhist: rloc = 2
     else: rloc = 1
@@ -199,41 +207,32 @@ def phaserate(hists,mids,exptime,bgb=False,bg=[],ens=['0.3-10 keV','S: 0.3-1.5 k
             rerr[r]=hre[r].s 
             hr[r]=hre[r].n
         hrbins = hists[0][1]
-        hrhist = ax[rloc].hist(hrbins[:-1],hrbins,weights=hr,color='slateblue',histtype='step',label='(H-S)/(H+S)')
+        hrhist = ax[rloc].hist(hrbins[:-1],hrbins,weights=hr,color='slateblue',histtype='step')
         ax[rloc].errorbar(mids,hr,yerr=rerr,linestyle='none',color='slateblue')
-        ax[rloc].legend()
-        ax[rloc].set_ylabel('Hardness Ratio',fontsize=18)
-        if enhist:
-            ax[rloc].set_ylim(-.2,.15)
-            ax[rloc].set_yticks(np.arange(-.2,.2,.05))
-            ax[rloc].xaxis.set_major_locator(MultipleLocator(0.5))
-            ax[rloc].xaxis.set_minor_locator(MultipleLocator(0.1))
+        #ax[rloc].legend()
+        ax[rloc].set_ylabel('HR',fontsize=18)
+#         if enhist:
+#             ax[rloc].set_ylim(-.2,.15)
+#             ax[rloc].set_yticks(np.arange(-.2,.2,.05))
+#             ax[rloc].xaxis.set_major_locator(MultipleLocator(0.5))
+#             ax[rloc].xaxis.set_minor_locator(MultipleLocator(0.1))
+        ax[rloc].set_ylim(yrange2)
+        ax[rloc].set_yticks(np.arange(yrange2[0],yrange2[1]+0.03,.1))
+        ax[rloc].set_xticks(np.arange(0,2.1,0.5))
+        ax[rloc].minorticks_on()
         #add hardness ratio errors  
     #puts bg-subtracted, normalized, smoothed energy-phase heat map
     if enhist:
-#         #need these defined
-#         ph = ed['TIME']%pd
-#         ph1 = ph/pd
-#         ph2 = ph1 + 1
-#         ph3 = list(ph1)+list(ph2)
-#         ed2 = list(ed['PI'])+list(ed['PI'])
-#         enhist = ax[1].hist2d(ph3,np.array(ed2)/100,bins=32) #convert from PI to keV
-#         #normalizing and background-subtracting
-#         norm_bg = np.zeros((32,32)) #16 phase bins
-#         #enbg defined in jupyter notebook
-#         for i in range(32):
-#             norm_bg[i,:] = ((enhist[0][:,31-i]*16/exptime)-enbg[31-i,1]) #-enbg[i,1]
-#             norm_bg[i,:] = norm_bg[i,:]/np.median(norm_bg[i,:]) #16 phase bins
-        im = ax[1].imshow(norm_bg,cmap='plasma',interpolation='gaussian',aspect='auto')
-        ax[1].set_xticks([])
+        norm_bg_fil=gaussian_filter(norm_bg, 0.8,mode='wrap')
+        im = ax[0].imshow(norm_bg_fil,cmap='plasma',interpolation='spline16',aspect='auto')
         #ax[1].set_xticks([-0.5,-.5+31/8,-.5+62/8,-.5+93/8,-.5+124/8,-.5+155/8,-.5+186/8,31.5-31/8,31.5])
         #just on multiples of 0.5 and then minor ticks
-        ax[1].set_xticks([-0.5,-.5+32/4,-.5+64/4,-.5+96/4,31.5])
-        a=ax[1].get_xticks().tolist()
+        ax[0].set_xticks([-0.5,-.5+32/4,-.5+64/4,-.5+96/4,31.5])
+        a=ax[0].get_xticks().tolist()
         #a=['0','0.25','0.5','0.75','1.0','1.25','1.50','1.75','2.0']
-        a=['0','0.5','1.0','1.50','2.0']
-        ax[1].set_xticklabels(a)
-        ax[1].set_xticks(np.arange(-.5,31.5,32/20), minor=True)
+        a=['','','','','']
+        ax[0].set_xticklabels(a)
+        ax[0].set_xticks(np.arange(-.5,31.5,32/20), minor=True)
         #trying to add minor ticks
         #ax[1].xaxis.set_major_locator(MultipleLocator(8))
 #         ax[1].xaxis.set_major_formatter(FormatStrFormatter('%d'))
@@ -242,23 +241,25 @@ def phaserate(hists,mids,exptime,bgb=False,bg=[],ens=['0.3-10 keV','S: 0.3-1.5 k
 #         a=['0','0','0.5','1.0','1.5','2.0']
 #         ax[1].set_xticklabels(a)
         
-        ax[1].set_yticks([28.5,28.5-32/7.7,28.5-2*32/7.7,28.5-3*32/7.7,28.5-4*32/7.7,28.5-5*32/7.7,28.5-6*32/7.7,28.5-7*32/7.7])
-        a=ax[1].get_yticks().tolist()
+        ax[0].set_yticks([28.5,28.5-32/7.7,28.5-2*32/7.7,28.5-3*32/7.7,28.5-4*32/7.7,28.5-5*32/7.7,28.5-6*32/7.7,28.5-7*32/7.7])
+        a=ax[0].get_yticks().tolist()
         a=['1','2','3','4','5','6','7','8']
-        ax[1].set_yticklabels(a)
-        ax[1].set_ylabel('Energy (0.3-8.0 keV)',fontsize=18)
+        ax[0].set_yticklabels(a)
+        ax[0].set_ylabel('Energy (0.3-8.0 keV)',fontsize=18)
         #fig.colorbar(im,ax=ax[1],label='Normalized Counts')
         from mpl_toolkits.axes_grid1.inset_locator import inset_axes
-        axins = inset_axes(ax[1],
-                   width="5%",  # width = 5% of parent_bbox width
-                   height="100%",  # height : 50%
-                   loc='lower left',
-                   bbox_to_anchor=(1.02, 0., 1, 1),
-                   bbox_transform=ax[1].transAxes,
+        axins = inset_axes(ax[0],
+                   width="100%",  # width = 5% of parent_bbox width
+                   height="5%",  # height : 50%
+                   loc='upper right',
+                   bbox_to_anchor=(0.0, 0.07, 1, 1),
+                   bbox_transform=ax[0].transAxes,
                    borderpad=0
                    )
         
-        cbar = fig.colorbar(im,cax=axins,orientation='vertical',pad=.01)
+        cbar = fig.colorbar(im,cax=axins,orientation='horizontal',pad=.02)
+        axins.xaxis.set_label_position('top')
+        axins.xaxis.set_ticks_position('top')
         #plt.colorbar().set_label(label='a label',size=15,weight='bold')
         cbar.set_label(label='Normalized Counts',size=18)
         #cbar.ax.tick_params() 
